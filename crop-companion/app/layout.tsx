7e4382791410c520841a611c4682b5ab023c6af5
Dhananjay Surti <djsurti3003@gmail.com>
2025-02-02 21:50:58 -0800

    Create a new page

--- crop-companion/app/layout.tsx
+++ crop-companion/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 export default function RootLayout({
   children,
@@ -10,6 +11,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Navbar />
         {children}
       </body>
     </html>
