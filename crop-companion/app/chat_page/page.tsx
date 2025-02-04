c4cfdc577cc79dfd659033ede1ae83dbaaa94e30
Dhananjay Surti <djsurti3003@gmail.com>
2025-02-03 16:10:49 -0800

    Sort of fix the issue

--- crop-companion/app/chat_page/page.tsx
+++ crop-companion/app/chat_page/page.tsx
@@ -23,7 +23,7 @@ export default function Page() {
     setInput("");
     setLoading(true);
     try{
-        const response = await fetch("https://localhost:8000", {
+        const response = await fetch("http://localhost:8000", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
