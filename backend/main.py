e49da6865536a139b3dae14e9c0f151a44111ee6
nkabembo <71999441+nkabembo@users.noreply.github.com>
2025-02-10 11:42:28 +0200

    ser up mongo db
    rebasing

--- backend/main.py
+++ backend/main.py
@@ -12,6 +12,9 @@ app.include_router(auth_routes.router)
 app.include_router(llm_routes.router)
 app.include_router(user_routes.router)
 
+#client = MongoClient(MONGO_URI)
+#db = client[DATABASE]
+# app.include_router(db_routes.router)
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
